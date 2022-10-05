3242ef05693d6e327dd57724c517d369e40d0fca
pnothop <nothop@posteo.de>
2022-10-05 16:34:17 +0200

    Secrets

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./service_worker.js");
 }
+
+const audio = new Audio("./hash.wav");
+
+let secretStep = 0;
+
+const rightStepTaken = (event) => {
+  if (
+    (secretStep === 0 || secretStep === 3) &&
+    event.clientX < window.innerWidth / 2
+  )
+    return true;
+  if (
+    (secretStep === 1 || secretStep === 2 || secretStep === 4) &&
+    event.clientX > window.innerWidth / 2
+  )
+    return true;
+
+  return false;
+};
+
+document.addEventListener("pointerdown", (event) => {
+  if (rightStepTaken(event)) {
+    secretStep++;
+  } else {
+    secretStep = 0;
+  }
+
+  if (secretStep === 5) {
+    // prettier-ignore
+    audio.addEventListener(
+      "ended",
+      () => window.location.href = "https://www.facebook.com/Reformschule.Kassel",
+      { once: true }
+    );
+    audio.play();
+
+    secretStep = 0;
+  }
+});
